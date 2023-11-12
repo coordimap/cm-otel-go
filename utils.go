74472cc24f1a15d7fd45a99e6be1cdc388b59ee5
Ermal Guni <ermalguni@gmail.com>
2023-11-12 09:27:56 +0100

    Create a function to get the unique service prefix

--- utils.go
+++ utils.go
@@ -12,21 +12,26 @@ func GetEnvWithPrefix(prefix string, env string) string {
 	return os.Getenv(envKey)
 }
 
-// GetServiceName returns the service name based on the envi variables being set. It first checks if the SERVICE_NAME_PREFIX env variable is set. If so that it generates the name by using the value as a prefix.
-// Otherwise if K8S_CLUSTER_NAME and NAMESPACE_NAME have been set then it uses both of them to generate the service name. It also takes into consideration the CM_PREFIX env variable when looking for the previously
-// mentioned environment variables. If none has been set then it returns the provided name.
+// GetServiceName returns the service name based on the unique service prefix.
 func GetServiceName(name string) string {
+	return fmt.Sprintf("%s%s", GetUniqueServicePrefix(), name)
+}
+
+// GetUniqueServicePrefix returns the prefix that will be used to name the service and any other remote services that are being called. It takes into account the environment variables being set.
+// It first checks if the SERVICE_NAME_PREFIX env variable is set. If so that it generates the name by using the value as a prefix. Otherwise if K8S_CLUSTER_NAME and NAMESPACE_NAME have been set
+// then it uses both of them to generate the service name. It also takes into consideration the CM_PREFIX env variable when looking for the previously mentioned environment variables. If none has been set then it returns the provided name.
+func GetUniqueServicePrefix() string {
 	envPrefix := os.Getenv(EnvCmPrefix)
 	servicePrefix := GetEnvWithPrefix(envPrefix, EnvServiceNamePrefix)
 	if servicePrefix != "" {
-		return fmt.Sprintf("%s%s", servicePrefix, name)
+		return servicePrefix
 	}
 
 	k8sClusterName := GetEnvWithPrefix(envPrefix, EnvK8SClusterName)
 	k8sNamespace := GetEnvWithPrefix(envPrefix, EnvK8SNamespaceName)
 	if k8sClusterName != "" && k8sNamespace != "" {
-		return fmt.Sprintf("%s%s%s", k8sClusterName, k8sNamespace, name)
+		return fmt.Sprintf("%s%s", k8sClusterName, k8sNamespace)
 	}
 
-	return name
+	return ""
 }
